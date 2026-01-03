746d3451882708c26fce34e866f46cf426e6382b
Anis SAIED <69865343+anis-saied@users.noreply.github.com>
2026-01-03 18:00:16 +0100

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -43,7 +43,7 @@ const config: Config = {
           sidebarPath: "./sidebars.ts",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/anis-saied/my-wiki/edit/main/",
+          editUrl: "https://github.com/anis-saied/my-wiki/tree/main/",
         },
         blog: {
           showReadingTime: true,
@@ -53,7 +53,7 @@ const config: Config = {
           },
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/anis-saied/my-wiki/edit/main/",
+          editUrl: "https://github.com/anis-saied/my-wiki/tree/main/",
           // Useful options to enforce blogging best practices
           onInlineTags: "warn",
           onInlineAuthors: "warn",
